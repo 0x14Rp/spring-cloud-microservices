7969c68ee54481a6979c29755132bdd4f4ce9e03
Rafael <elgondu@gmail.com>
2020-05-09 18:46:00 -0400

    init commit proyect

---
+++ microservices/src/test/java/com/app/products/serviceproducts/ServiceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.products.serviceproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

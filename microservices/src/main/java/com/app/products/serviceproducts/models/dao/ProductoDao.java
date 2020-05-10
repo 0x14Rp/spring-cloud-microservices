a1df81a4863b4c58ed3fc8c4af977757ee87c474
Rafael <elgondu@gmail.com>
2020-05-10 14:04:19 -0400

    refactor

--- microservices/src/main/java/com/app/products/serviceproducts/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.products.serviceproducts.models.dao;
-
-import com.app.products.serviceproducts.models.entity.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductoDao extends CrudRepository<Producto,Long> {
-
-
-}

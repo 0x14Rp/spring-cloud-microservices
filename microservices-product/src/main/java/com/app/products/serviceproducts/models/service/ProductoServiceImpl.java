381c484eab0dc64e0d32635a9bcc2d76b1d88570
Rafael <elgondu@gmail.com>
2020-05-10 14:07:46 -0400

    refactor for new folder

---
+++ microservices-product/src/main/java/com/app/products/serviceproducts/models/service/ProductoServiceImpl.java
@@ -0,0 +1,28 @@
+package com.app.products.serviceproducts.models.service;
+
+import com.app.products.serviceproducts.models.dao.ProductoDao;
+import com.app.products.serviceproducts.models.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements IProductosService {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findAll() {
+        return (List<Producto>) productoDao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto findById(Long id) {
+        return productoDao.findById(id).orElse(null);
+    }
+}

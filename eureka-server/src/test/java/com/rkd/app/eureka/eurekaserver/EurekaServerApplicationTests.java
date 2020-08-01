460dc56fcd2e49ce4b3f17a54235c1efae579fd7
elgondu@gmail.com <elgondu@gmail.com>
2020-08-01 18:22:07 -0400

    created eureka server

---
+++ eureka-server/src/test/java/com/rkd/app/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkd.app.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
